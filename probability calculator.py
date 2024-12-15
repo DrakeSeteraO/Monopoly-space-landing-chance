77d8b32c691e241a0960ccb13d1c1e0d1653cdd6
Drake Setera <47800977+DrakeSeteraO@users.noreply.github.com>
2024-12-15 14:46:52 -0500

    Add files via upload

--- probability calculator.py
+++ probability calculator.py
@@ -1,51 +1,53 @@
-def main():
-    turns = 30
-    amount = [0] * (turns * 12)
-    amount = first_12(amount)
-    for i in range(13,len(amount)):
-        a = amount[i-2]
-        b = 2 * amount[i-3]
-        c = 3 * amount[i-4]
-        d = 4 * amount[i-5]
-        e = 5 * amount[i-6]
-        f = 6 * amount[i-7]
-        g = 5 * amount[i-8]
-        h = 4 * amount[i-9]
-        j = 3 * amount[i-10]
-        k = 2 * amount[i-11]
-        l = amount[i-12]
-        amount[i] = a + b + c + d + e + f + g + h + j + k + l
-    
-    total = sum(amount)
-    total_of_each_space = [0] * 40
-    for i in range(len(amount)):
-        total_of_each_space[i % 40] += amount[i]
-    
-    average_of_each_space = [0] * 40
-    amount_to_space = dict()
-    for i in range(len(total_of_each_space)):
-        average_of_each_space[i] = total_of_each_space[i] / total
-        amount_to_space[average_of_each_space[i]] = i
-    
-    average_of_each_space.sort(reverse=True)
-    print('The most common spaces from most likely to least likely are')
-    for val in average_of_each_space:
-        percentage = val * 100
-        print(f'Space: {amount_to_space[val]}\tPercentage: {percentage} %')
-        
-def first_12(lst: list) -> list:
-    lst[2] = 1
-    lst[3] = 2
-    lst[4] = 4
-    lst[5] = 8
-    lst[6] = 16
-    lst[7] = 32
-    lst[8] = 62
-    lst[9] = 124
-    lst[10] = 246
-    lst[11] = 488
-    lst[12] = 968
+def create_list(turns: int) -> list:
+    lst = [[0 for i in range(turns)] for j in range(turns * 12)]
+    for i in range(7):
+        lst[i][0] = i
+    for i in range(7):
+        lst[i+6][0] = 6 - i
     return lst
+
+
+def calculate_amount(lst: list, turns: int) -> list:
+    for i in range(turns * 12):
+        for j in range(turns - 1):
+            total = 0
+            if lst[i][j + 1] == 0:
+                for k in range(1, 12):
+                    l = i - k - 1
+                    if 0 <= l:
+                        if k < 6:
+                            total += lst[l][j] * k
+                        else:
+                            total += lst[l][j] * (12 - k)
+                lst[i][j + 1] = total
+    return lst
+
+
+def calculate_totals(lst: list):
+    sums = [0] * 40
+    for i in range(len(lst)):
+       sums[i % 40] += sum(lst[i]) 
+    return sums, sum(sums)
+
+
+def create_dictionary(sums):
+    out = dict()
+    for i in range(len(sums)):
+        out[sums[i]] = i
+    return out
+
+def main():
+    turns = int(input('How many turns would you like to calculate for? '))
+    amount = create_list(turns)
+    amount = calculate_amount(amount, turns)
+    sums, total = calculate_totals(amount)
+    amount_to_space = create_dictionary(sums)
+    sums.sort(reverse=True)
+    print(f'\nAfter calculating all the possibilities for {turns} turns\nThe results are:\n\t\tTotal: {total:>108,}')
+    for s in sums:
+        print(f'Space: {amount_to_space[s]}\tpossibilities: {s:>100,}')
+            
+    
 
 if __name__ == '__main__':
     main()
